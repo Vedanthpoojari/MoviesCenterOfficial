adfe4b2a7f018c0975b9ab475a2cf94578b850a2
Vedanthpoojari <97805128+Vedanthpoojari@users.noreply.github.com>
2023-01-09 19:57:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/watch_onli8e/6"
+DOWNLOAD_TEXT_URL = "https://t.me/watch_onli8e/7"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Get Updates"
